abf25fb55b0b4b3e40e224dbeb155bdf4aafe29a
McWooden <garryf7887@gmail.com>
2022-09-10 23:35:02 +0700

    costum again becouse more error :)

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,5 @@
-let tugas = []
-const myChache = 'todolist'
-
-
 // window on load
 window.addEventListener('load', () => {
-    ambilProggress()
-    // document.dispatchEvent(new Event('renderTugas'))
     greet()
     getDate()
 })
@@ -18,15 +12,6 @@ document.addEventListener('renderTugas', () => {
     simpanProggress()
     updateProggress()
 })
-
-// animate btn submit 
-function rotateSubmitButton() {
-    const btnSubmit = document.getElementById('submitImg')
-    btnSubmit.style.animation = 'rotate .3s'
-    setTimeout(() => {
-        btnSubmit.style.animation = ''
-    }, 300)
-}
 
 fetch("https://jservice.io/api/random")
 .then(res => res.json())
@@ -41,17 +26,6 @@ fetch("https://jservice.io/api/random")
 // styling
 // make footer marginBottom = height nav
 document.getElementById('footer').style.marginBottom = (document.getElementById('nav').offsetHeight + 15) + 'px'
-
-// update proggres
-function updateProggress() {
-    if (tugas.length === 0) {
-        document.getElementById('proggress').style.display = 'none'
-    }
-    const tugasSelesai = tugas.filter(x => !x.selesai)
-    const tugasBelum = tugas.filter(x => x.selesai)
-    document.getElementById('valueBar').style.width = Math.round((tugasSelesai.length / tugas.length)*100) + '%'
-    document.getElementById('valueBarRed').style.width = Math.round((tugasBelum.length / tugas.length)*100) + '%'
-}
 
 // greet
 function greet() {
@@ -71,55 +45,6 @@ function getDate() {
     document.getElementById('day').innerText = dayName[new Date().getDay()]
     document.getElementById('month').innerText = monthName[new Date().getMonth()]
     document.getElementById('year').innerText = new Date().getFullYear()
-}
-
-// edit area
-function editCard(x) {
-    formState.isEdit = true
-    document.getElementById('tugas').value = tugas[x].tugas
-    document.getElementById('deskripsi').value = tugas[x].deskripsi
-    document.getElementById('mulai').value = tugas[x].mulai
-    document.getElementById('tanggal').value = tugas[x].berakhir
-    document.getElementById('color').value = tugas[x].color
-    if (document.getElementById('color').value == '#000000') {
-        document.getElementById('color').value = '#31364c'
-    }
-    document.getElementById('btnUpdate').style.visibility = 'visible'
-    document.getElementById('btnUpdate').dataset.key = x
-    getEditStatus(x)
-}
-document.getElementById('btnUpdate').addEventListener('click', (e) => {
-    e.preventDefault()
-    saveEdit(document.getElementById('btnUpdate').dataset.key)
-    kembalikanKeDefault()
-})
-function getEditStatus(x) {
-    document.getElementById('dateNow').innerText = x
-    document.getElementById('day').innerText = tugas[x].tugas
-    document.getElementById('month').innerText = '#' + tugas[x].id
-    document.getElementById('year').innerText = tugas[x].selesai
-    document.getElementById('greet').innerText = 'Mengubah data:' 
-}
-// save edit
-function saveEdit(x) {
-    tugas[x].tugas = document.getElementById('tugas').value
-    tugas[x].deskripsi = document.getElementById('deskripsi').value
-    tugas[x].color = document.getElementById('color').value
-    tugas[x].mulai = document.getElementById('mulai').value
-    tugas[x].berakhir = document.getElementById('tanggal').value
-    // document.dispatchEvent(new Event('renderTugas'))
-    popup(alertMsg.save)
-}
-function kembalikanKeDefault() {
-    document.getElementById('tugas').value = ''
-    document.getElementById('deskripsi').value = ''
-    document.getElementById('mulai').value = ''
-    document.getElementById('tanggal').value = ''
-    document.getElementById('color').value = '#31364c'
-    greet()
-    getDate()
-    formState.isEdit = false
-    document.getElementById('btnUpdate').style.visibility = 'hidden'
 }
 
 // pop up :)
