3f73028776940bf3fa592777872b6d8c71b84a92
McWooden <garryf7887@gmail.com>
2022-10-01 23:15:50 +0700

    bug wrong url callback

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 passport.use(new GoogleStrategy({
     clientID: '701484835392-moov4mk232l02t6mtklkat5ncpp67iek.apps.googleusercontent.com',
     clientSecret: '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',
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://x6todo.herokuapp.com/google/callback",
     scope: ['profile']
   },
   function(accessToken, refreshToken, profile, done) {
